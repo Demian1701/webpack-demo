bc61f2f234c75b256b3a37a0080dd4b6cb875790
Demian1701 <fioranidemian@gmail.com>
2021-03-31 11:46:51 -0300

    agregamos 11th exito

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ function component() {
   element.appendChild(botonaso);
   const shadowContent = document.createElement("section");
   shadowContent.className = "shadow-content";
-  shadowContent.attachShadow({ mode: "open" });
   element.appendChild(shadowContent);
   // var shadow = this.attachShadow({mode: 'open'});
   console.log(shadowContent);
@@ -23,30 +22,61 @@ documentaso.appendChild(component());
 let botonaso = document.querySelector("#botonaso");
 
 botonaso.addEventListener("click", function createShadow() {
-  var style = document.createElement('style');
+  var style = document.createElement("style");
 
-  style.textContent = `
-  #botonasoo{
-    background-color:green;
-  }
-  p{
-    color:yellow;
-  }`;
-  
+  // style.textContent = `
+  // #botonasoo{
+  //   background-color:green;
+  // }
+  // p{
+  //   color:yellow;
+  // }`;
+
   const shadowContent = document.querySelector(".shadow-content");
+  var shadow = shadowContent.attachShadow({ mode: "closed" });
   // let shadow = shadowContent.attachShadow({ mode: "open" });
-  shadowContent.appendChild(style);
-  var para = document.createElement("p");
-  para.textContent = "Hola";
-  shadowContent.appendChild(para);
-  const botonaso = document.createElement("button");
-  botonaso.id = "botonasoo";
-  botonaso.textContent = "shadoow?";
-  shadowContent.appendChild(botonaso);
-  // console.log(shadow);
-  console.log(shadowContent);
-
+  // var para = document.createElement("p");
+  // para.textContent = "Hola";
+  // shadow.appendChild(para);
+  // const botonaso = document.createElement("button");
+  // botonaso.id = "botonasoo";
+  // botonaso.textContent = "shadoow?";
+  // shadow.appendChild(botonaso);
+  // // console.log(shadow);
+  // console.log(shadowContent);
   
+  var urlCSS =
+  "https://s3-sa-east-1.amazonaws.com/api.sis/resource/css/styles-11th-exito.css";
+  
+  fetch(urlCSS)
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (csss) {
+    var style = document.createElement("style");
+    
+    style.textContent = csss;
+    // Convert the HTML string into a document object
+    // var parser = new DOMParser();
+    // var documentito = parser.parseFromString(string, "text/html");
+    // shadow.appendChild(documentito.documentElement);
+    console.log(csss);
+    shadow.appendChild(style);
+    });
+
+  var urlHTML =
+    "https://s3-sa-east-1.amazonaws.com/api.sis/resource/html/pages/gen11th_exito.html";
+
+  fetch(urlHTML)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (string) {
+      // Convert the HTML string into a document object
+      var parser = new DOMParser();
+      var documentito = parser.parseFromString(string, "text/html");
+      shadow.appendChild(documentito.documentElement);
+    });
 
   // var url =
   //   "https://ppintel.s3-sa-east-1.amazonaws.com/assets/1228_LARDigital_Q320/7924/preview-6/index.html";
